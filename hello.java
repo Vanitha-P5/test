8c57a79f0fdab885c4a9e959ca57491442277634
vanitha <vanithabe2018@gmail.com>
2021-10-18 12:12:42 -0700

    Third Commit

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public class hello
 public static void main(String[] args){
 System.out.println("Hello,World from GIT!");
 System.out.println("Welcome to GIT!");
+System.out.println("Changes after pushing to remote");
 }
 }
